2daaae5d44bdabc73b5a500f20afe15452b24b5f
juliomiguell <juliomiguelsouzacosta@gmail.com>
2020-05-24 20:53:13 -0300

    primeiro contato com electron :smile:

---
+++ index.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 400,
+        height: 300,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('./src/index.html');
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin')
+    app.quit()
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0)
+    createWindow()
+})
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Olá mundo em electron
+
+## Meu primeiro contato com electron
+
+### Captura de tela
+![alt text](images/demo.png)
